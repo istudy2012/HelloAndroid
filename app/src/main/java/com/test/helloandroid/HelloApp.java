33a2c333f18c50ee51b5c674bd7dde98dbd6028b
shiq <shiqfish@gmail.com>
2019-10-16 22:13:33 +0800

    Config: Update Module config

---
+++ app/src/main/java/com/test/helloandroid/HelloApp.java
@@ -0,0 +1,16 @@
+package com.test.helloandroid;
+
+import android.app.Application;
+import android.content.Context;
+
+import androidx.multidex.MultiDex;
+
+public class HelloApp extends Application {
+
+    @Override
+    protected void attachBaseContext(Context base) {
+        super.attachBaseContext(base);
+
+        MultiDex.install(this);
+    }
+}
