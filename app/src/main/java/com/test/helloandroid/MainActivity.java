315ea6ade73490cfc406113fcd77662116982280
shiq <shiqfish@gmail.com>
2019-10-30 23:40:05 +0800

    Update: add Bus logic

--- app/src/main/java/com/test/helloandroid/MainActivity.java
+++ app/src/main/java/com/test/helloandroid/MainActivity.java
@@ -1,7 +1,9 @@
 package com.test.helloandroid;
 
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.TextView;
 import android.widget.Toast;
 
 import com.test.helloandroid.business.wanandroid.presenter.MainPresenter;
@@ -9,6 +11,9 @@ import com.test.lib.event.mybus.Event;
 import com.test.lib.event.mybus.EventHandler;
 
 import androidx.appcompat.app.AppCompatActivity;
+
+import java.util.ArrayList;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -19,7 +24,47 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        EventHandler.register(this);
+        final TextView statusView = findViewById(R.id.status);
+
+        final List<EventTest> list = new ArrayList<>();
+
+        findViewById(R.id.register).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                for (int i = 0; i < 100000; i++) {
+                    EventTest eventTest = new EventTest(i);
+                    list.add(eventTest);
+                }
+
+                Log.d("test", "register start: ");
+                long startTime = System.currentTimeMillis();
+
+                for (EventTest test : list) {
+                    test.register();
+                }
+
+                Log.d("test", "register end: " + (System.currentTimeMillis() - startTime));
+                statusView.setText("Registered");
+            }
+        });
+
+        findViewById(R.id.unregister).setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Log.d("test", "unregister start: ");
+                long startTime = System.currentTimeMillis();
+
+                for (EventTest test : list) {
+                    test.unregister();
+                }
+
+                Log.d("test", "unregister end: " + (System.currentTimeMillis() - startTime));
+
+                statusView.setText("Unregister");
+
+                list.clear();
+            }
+        });
 
         findViewById(R.id.button).setOnClickListener(new View.OnClickListener() {
             @Override
@@ -30,12 +75,38 @@ public class MainActivity extends AppCompatActivity {
         });
     }
 
+    private int count = 0;
+
     @Event
     public void handle(EventData eventData) {
-        Toast.makeText(this, "handle event", Toast.LENGTH_SHORT).show();
+        count++;
+        Toast.makeText(this, "handle event: " + count, Toast.LENGTH_SHORT).show();
     }
 
     private class EventData {
+
+    }
+
+    private class EventTest {
+
+        private int id;
+
+        public EventTest(int id) {
+            this.id = id;
+        }
+
+        public void register() {
+            EventHandler.register(this);
+        }
+
+        public void unregister() {
+            EventHandler.unregister(this);
+        }
+
+        @Event
+        public void onEvent(EventData data) {
+            Log.d("event", "event receive: " + id);
+        }
 
     }
 }
--- lib/src/main/java/com/test/lib/event/mybus/EventHandler.java
+++ lib/src/main/java/com/test/lib/event/mybus/EventHandler.java
@@ -1,19 +1,32 @@
 package com.test.lib.event.mybus;
 
+import android.util.Log;
+
 import com.test.lib.log.HLog;
 
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
-import java.lang.reflect.Parameter;
 import java.util.ArrayList;
 import java.util.HashMap;
+import java.util.HashSet;
+import java.util.Iterator;
 import java.util.List;
 import java.util.Map;
+import java.util.Set;
 
 public class EventHandler {
     private static final Map<Class, List<EventItem>> eventMap = new HashMap<>();
+    private static final Set<Object> objectSet = new HashSet<>();
 
     public static void register(Object object) {
+        if (object == null) {
+            return;
+        }
+
+        if (objectSet.contains(object)) {
+            return;
+        }
+
         List<EventItem> methodList = getEventItems(object);
         for (EventItem methodItem : methodList) {
             if (eventMap.containsKey(methodItem.paramCls)) {
@@ -24,19 +37,48 @@ public class EventHandler {
                 eventMap.put(methodItem.paramCls, newItems);
             }
         }
+        objectSet.add(object);
     }
 
     public static void unregister(Object object) {
+        if (object == null) {
+            return;
+        }
 
+        if (!objectSet.contains(object)) {
+            return;
+        }
+
+        List<Class> eventClassList = getEventClassList(object);
+        for (Class eventClass : eventClassList) {
+            if (eventMap.containsKey(eventClass)) {
+                List<EventItem> eventItems = eventMap.get(eventClass);
+                if (eventItems != null && !eventItems.isEmpty()) {
+                    Iterator<EventItem> iterable = eventItems.iterator();
+                    while (iterable.hasNext()) {
+                        EventItem item = iterable.next();
+                        if (item.object == object) {
+                            iterable.remove();
+                        }
+                    }
+                }
+            }
+        }
+        objectSet.remove(object);
     }
 
     public static void sendEvent(Object object) {
+        Log.d("test", "sendEvent start: ");
+        long startTime = System.currentTimeMillis();
+
         if (eventMap.containsKey(object.getClass())) {
             List<EventItem> eventItems = eventMap.get(object.getClass());
             for (EventItem item : eventItems) {
                 item.invoke(object);
             }
         }
+
+        Log.d("test", "sendEvent end: " + (System.currentTimeMillis() - startTime));
     }
 
     private static List<EventItem> getEventItems(Object object) {
@@ -54,6 +96,22 @@ public class EventHandler {
         }
 
         return eventMethods;
+    }
+
+    private static List<Class> getEventClassList(Object object) {
+        Method[] methods = object.getClass().getDeclaredMethods();
+        List<Class> eventClassList = new ArrayList<>();
+
+        for (Method method : methods) {
+            if (method.isAnnotationPresent(Event.class)) {
+                Class[] params = method.getParameterTypes();
+                if (params.length == 1) {
+                    eventClassList.add(params[0]);
+                }
+            }
+        }
+
+        return eventClassList;
     }
 
     public static class EventItem {
